e11afe912fff9afe03a4bcf138b4b032269a02a4
cuberhaus <polcg10@gmail.com>
2022-09-18 19:02:33 +0200

    better structured python code

--- sesio1/Final codes/Zipf Final/filter_words.py
+++ sesio1/Final codes/Zipf Final/filter_words.py
@@ -1,30 +1,35 @@
-import pandas as pd
-import datetime
-import matplotlib
 import argparse
 import os
+
+import pandas as pd
 from tqdm import tqdm
 
-parser = argparse.ArgumentParser()
-path_words = parser.parse_args().path
 
-# Read file
-parser.add_argument('--path', required=True, default=None, help='Path to the files')
-df = pd.read_csv(path_words, sep=", ", header=0)
+def main():
+    parser = argparse.ArgumentParser()
+    path_words = parser.parse_args().path
 
-# We erase the last two rows
-df = df.iloc[:-2]
+    # Read file
+    parser.add_argument('--path', required=True, default=None, help='Path to the files')
+    df = pd.read_csv(path_words, sep=", ", header=0)
 
-# Filter out non alpha "words"
-for index, row in tqdm(df.iterrows(), "Rows:"):
-    word = str(row[1])  # Must convert to string cause there are some floats
-    if not word.isalpha():
-        df.drop(index=index, inplace=True)
+    # We erase the last two rows
+    df = df.iloc[:-2]
 
-# Convert string to int
-df[df.columns[0]] = pd.to_numeric(df[df.columns[0]], errors='coerce')
+    # Filter out non alpha "words"
+    for index, row in tqdm(df.iterrows(), "Rows:"):
+        word = str(row[1])  # Must convert to string cause there are some floats
+        if not word.isalpha():
+            df.drop(index=index, inplace=True)
 
-# Sort in descending order
-df.sort_values(by=df.columns[0], ascending = False, inplace = True)    
+    # Convert string to int
+    df[df.columns[0]] = pd.to_numeric(df[df.columns[0]], errors='coerce')
 
-df.to_csv("./data_" + str(os.path.splitext(path_words)[0]) + ".csv", index=False, header=False, sep='\t')
+    # Sort in descending order
+    df.sort_values(by=df.columns[0], ascending=False, inplace=True)
+
+    df.to_csv("./data_" + str(os.path.splitext(path_words)[0]) + ".csv", index=False, header=False, sep='\t')
+
+
+if __name__ == '__main__':  # Executed when invoked directly, not when imported
+    main()
