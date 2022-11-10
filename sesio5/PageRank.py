ff388b3644a742ee5184dae0a7930916009d7982
cuberhaus <polcg10@gmail.com>
2022-11-10 12:18:46 +0100

    formating

--- sesio5/PageRank.py
+++ sesio5/PageRank.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python
 
-from collections import namedtuple
-import time
 import sys
+import time
+
 
 class Edge:
-    def __init__ (self, origin=None):
-        self.origin = origin # write appropriate value
-        self.weight = 1.0 # write appropriate value
+    def __init__(self, origin=None):
+        self.origin = origin  # write appropriate value
+        self.weight = 1.0  # write appropriate value
+
     def __repr__(self):
         return "edge: {0} {1}".format(self.origin, self.weight)
-        
-    ## write rest of code that you need for this class
+
+    # write rest of code that you need for this class
+
 
 class Airport:
-    def __init__ (self, iden=None, name=None, indx=None):
+    def __init__(self, iden=None, name=None, indx=None):
         self.code = iden
         self.name = name
         self.routes = []
         self.routeHash = dict()
-        self.outweight = 0.0   # write appropriate value
+        self.outweight = 0.0  # write appropriate value
         self.indx = indx
-        
+
     def addInEdge(self, inAirport):
         if inAirport in self.routeHash:
             edge = self.routeHash[inAirport]
@@ -34,26 +36,28 @@ class Airport:
     def __repr__(self):
         return f"{self.code}\t{self.pageIndex}\t{self.name}"
 
-edgeList = [] # list of Edge
-edgeHash = dict() # hash of edge to ease the match
-airportList = [] # list of Airport
-airportHash = dict() # hash key IATA code -> Airport
-pageRank = [] # list of page rank
+
+edgeList = []  # list of Edge
+edgeHash = dict()  # hash of edge to ease the match
+airportList = []  # list of Airport
+airportHash = dict()  # hash key IATA code -> Airport
+pageRank = []  # list of page rank
+
 
 def readAirports(fd):
     print("Reading Airport file from {0}".format(fd))
-    airportsTxt = open(fd, "r");
+    airportsTxt = open(fd, "r")
     cont = 0
     for line in airportsTxt.readlines():
         a = Airport()
         try:
             temp = line.split(',')
-            if len(temp[4]) != 5 :
+            if len(temp[4]) != 5:
                 raise Exception('not an IATA code')
-            a.name=temp[1][1:-1] + ", " + temp[3][1:-1]
-            a.code=temp[4][1:-1]
+            a.name = temp[1][1:-1] + ", " + temp[3][1:-1]
+            a.code = temp[4][1:-1]
             a.indx = cont
-#            print("CONT=====================" + str(cont))
+        #            print("CONT=====================" + str(cont))
         except Exception as inst:
             pass
         else:
@@ -67,7 +71,7 @@ def readAirports(fd):
 def readRoutes(fd):
     print("Reading Routes file from {0}".format(fd))
     # write your code
-    routesTxt = open(fd, "r");
+    routesTxt = open(fd, "r")
     cont = 0
     for line in routesTxt.readlines():
         try:
@@ -76,10 +80,10 @@ def readRoutes(fd):
                 raise Exception('not an IATA code')
             origin = temp[2]
             destination = temp[4]
-            if not(origin in airportHash) or not(destination in airportHash):
-            	raise Exception('The airport does not exist')
-            airportHash[destination].addInEdge(origin)		
-            airportHash[origin].outweight += 1.0 		
+            if not (origin in airportHash) or not (destination in airportHash):
+                raise Exception('The airport does not exist')
+            airportHash[destination].addInEdge(origin)
+            airportHash[origin].outweight += 1.0
         except Exception as inst:
             pass
         else:
@@ -87,59 +91,61 @@ def readRoutes(fd):
     routesTxt.close()
     print(f"There were {cont} Edges with IATA code")
 
+
 def computePageRanks():
     # write your code
     n = len(airportHash)
-    P = [1/n]*n 
+    P = [1 / n] * n
     L = 0.9
     stop = False
-    aux1 = (1.0 - L)/n
-    aux2 = 1/n 
-    
+    aux1 = (1.0 - L) / n
+    aux2 = 1 / n
+
     # Calculating the disconnected nodes in the page rank (In pagerank.pdf, Important points number 3)
     listDisconected = []
     for element in airportList:
-    	if element.outweight == 0.0:
-    	   listDisconected.append(element)
-    	   
-    
+        if element.outweight == 0.0:
+            listDisconected.append(element)
+
     disconectedNodes = len(listDisconected)
-    totalDisconected = L/float(n)*disconectedNodes
-    
-    iteration = 0;
+    totalDisconected = L / float(n) * disconectedNodes
+
+    iteration = 0
     while not stop:
-        Q = [0.0]*n
+        Q = [0.0] * n
         for i in range(n):
             airport = airportList[i]
             suma = 0
-#            print("KKKKKKKKKKKKKKKK===========" + str(airport.routes))
-            for air,edge in airport.routeHash.items():
-#                print("EEEEEEEEEEE===========" + str(air))
-#                print("WWWWWWWWWWW===========" + str(edge))
-#                print("CODE+++++++++=========" + str(airportHash[air].code))
-                weight = edge.weight #w(j,i)
-                out = airportHash[air].outweight #out(j)
-                suma += P[airportHash[air].indx] * weight / out  #sum { P[j] * w(j,i) / out(j) : there is an edge (j,i) in G }
-            Q[i] = L * suma + aux1 + totalDisconected*aux2
-            
-        aux2 = aux1 + aux2*totalDisconected
-        # We create tuples of (first element of P, first element of Q), (second element of P, second element of Q) and we substract them
-        # to check if it complies with the stopping condition
-        for x, y in zip(P,Q):
-        	if (abs(x-y) <= 10**(-14)):
-        		stop = True
-        	else: stop = False	
-        	
+            #            print("KKKKKKKKKKKKKKKK===========" + str(airport.routes))
+            for air, edge in airport.routeHash.items():
+                #                print("EEEEEEEEEEE===========" + str(air))
+                #                print("WWWWWWWWWWW===========" + str(edge))
+                #                print("CODE+++++++++=========" + str(airportHash[air].code))
+                weight = edge.weight  # w(j,i)
+                out = airportHash[air].outweight  # out(j)
+                suma += P[airportHash[
+                    air].indx] * weight / out  # sum { P[j] * w(j,i) / out(j) : there is an edge (j,i) in G }
+            Q[i] = L * suma + aux1 + totalDisconected * aux2
 
-#        if iteration == 100: stop = True
+        aux2 = aux1 + aux2 * totalDisconected
+        # We create tuples of (first element of P, first element of Q), (second element of P, second element of Q)
+        # and we substract them to check if it complies with the stopping condition
+        for x, y in zip(P, Q):
+            if abs(x - y) <= 10 ** (-14):
+                stop = True
+            else:
+                stop = False
+
+        #        if iteration == 100: stop = True
         P = Q
         # Does P sums 1 ?
         print("SUM:" + str(sum(P)))
-        iteration += 1	
-    
+        iteration += 1
+
     global pageRank
-    pageRank = P   
-    return iteration;    
+    pageRank = P
+    return iteration
+
 
 def outputPageRanks():
     # List where we will save the tuple (airport_name, page_rank)
@@ -150,13 +156,14 @@ def outputPageRanks():
         mylist.append(element)
         j += 1
     # Sort by decreasing page rank    
-    mylist.sort(key = lambda x: x[1], reverse = True)
-    
-    print (" ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ ( Page rank, Airport name) ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ ")
-	
+    mylist.sort(key=lambda x: x[1], reverse=True)
+
+    print(" ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ ( Page rank, Airport name) ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ ")
+
     for air, pageR in mylist:
-        print ('(' + str(pageR) + ", " + str(air) + ')')
-		
+        print('(' + str(pageR) + ", " + str(air) + ')')
+
+
 def main(argv=None):
     readAirports("airports.txt")
     readRoutes("routes.txt")
@@ -165,7 +172,7 @@ def main(argv=None):
     time2 = time.time()
     outputPageRanks()
     print("#Iterations:", iterations)
-    print("Time of computePageRanks():", time2-time1)
+    print("Time of computePageRanks():", time2 - time1)
 
 
 if __name__ == "__main__":
