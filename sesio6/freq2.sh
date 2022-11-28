5464256bcd8b0091339c327f235e198985732152
cuberhaus <polcg10@gmail.com>
2022-11-28 17:26:32 +0100

    added experiments

--- sesio6/freq2.sh
+++ sesio6/freq2.sh
@@ -1,11 +1,12 @@
 m_freq=(0.1,0.3 0.3,0.5 0.5,0.7 0.7,0.9)
 n=${#sizes[@]}
-
+count=0
 for i in ${m_freq[@]}; do
     IFS=","
     set $i
-    echo "python ExtractData.py --index news --minfreq $1 --maxfreq $2 --numwords 200 --name $i &"
-    python ExtractData.py --index news --minfreq $1 --maxfreq $2 --numwords 200 --name $i &
+    echo "python ExtractData.py --index news --minfreq $1 --maxfreq $2 --numwords 200 --name $count &"
+    python ExtractData.py --index news --minfreq $1 --maxfreq $2 --numwords 200 --name $count &
+    count=$((count+1))
 done
 
 wait # This will wait for all child tasks to finish
