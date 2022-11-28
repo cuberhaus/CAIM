cbe2c7aa0ad78443cd9ce190f151f484a225fc09
cuberhaus <polcg10@gmail.com>
2022-11-28 22:36:10 +0100

    This is a placeholder

--- sesio6/scripts/size.sh
+++ sesio6/scripts/size.sh
@@ -5,13 +5,10 @@ for ((i = 0; i < n; i++)); do
     echo "python ExtractData.py --index news --minfreq 0.1 --maxfreq 0.3 --numwords $size --name $i &"
     python ExtractData.py --index news --minfreq 0.1 --maxfreq 0.3 --numwords "$size" --name "$i" &
 done
-
 wait # This will wait for all child tasks to finish
-# clusters=(2 4 8 16 32)
 for ((i = 0; i < n; i++)); do
     echo "python generateprototypes.py --data documents$i.txt &"
     python GeneratePrototypes.py --data documents"$i".txt &
 done
-
 wait
 echo "Program ended successfully"
