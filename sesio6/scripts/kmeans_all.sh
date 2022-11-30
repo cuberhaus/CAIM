3c036939f57fa76ad56905f193195b228f7a3644
Pol Casacuberta <polcg10@gmail.com>
2022-11-30 15:15:24 +0100

    kmeans_all

--- sesio6/scripts/kmeans_all.sh
+++ sesio6/scripts/kmeans_all.sh
@@ -1,4 +1,8 @@
 # This command lines cannot be executed in parallel, because they are dependent on each other.
+# Run elasticsearch:
+# docker run -p 127.0.0.1:9200:9200 -p 127.0.0.1:9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.17.7
+# Index the data:
+# python3 IndexFiles.py --index abs --path arxiv_abs
 ./scripts/clusters.sh
 ./scripts/freq.sh
 ./scripts/freq2.sh
