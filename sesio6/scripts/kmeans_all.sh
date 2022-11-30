67fc973578679afd929b8052d1ce58e359ee482b
Pol Casacuberta <polcg10@gmail.com>
2022-11-30 17:50:34 +0100

    This is a placeholder

--- sesio6/scripts/kmeans_all.sh
+++ sesio6/scripts/kmeans_all.sh
@@ -4,14 +4,23 @@
 # Index the data:
 # python3 IndexFiles.py --index abs --path arxiv_abs
 ./scripts/clusters.sh
+#wait
 ./scripts/freq.sh
+#wait
 ./scripts/freq2.sh
+#wait
 ./scripts/size.sh
+#wait
 
 ./scripts/kmeans.sh -n size
+#wait
 ./scripts/kmeans.sh -n freq
+#wait
 ./scripts/kmeans.sh -n freq2
+#wait
 ./scripts/kmeans_cluster.sh
+#wait
 ./scripts/results.sh
+#wait
 me=$(basename "$0")
 echo "${me} ended successfully"
