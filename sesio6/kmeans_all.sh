4c3751f30e9af137d3132729ebcf950f466c23d0
Pol Casacuberta <polcg10@gmail.com>
2022-11-30 13:19:36 +0100

    kmeans accepts arguments now

---
+++ sesio6/kmeans_all.sh
@@ -0,0 +1,8 @@
+./scripts/kmeans.sh -n size
+wait
+./scripts/kmeans.sh -n freq
+wait
+./scripts/kmeans.sh -n freq2
+wait
+./scripts/kmeans_cluster.sh
+wait
--- sesio6/scripts/kmeans.sh
+++ sesio6/scripts/kmeans.sh
@@ -1,12 +1,50 @@
 # Per executar un experiment diferent descomentar les dues línies exp,n i comentar les anteriors
+# list of arguments expected in the input
+
+exp=""                                   
+TIMES=1                                  
+usage() {                                 
+    echo "Usage: $0 [ -n EXPERIMENT_NAME ]" 1>&2
+}
+exit_abnormal() {                         # Function: Exit with error.
+    usage
+    exit 1
+}
+#https://www.computerhope.com/unix/bash/getopts.htm
+while getopts ":n:" options; do         # Loop: Get the next option;
+    # use silent error checking;
+    # options n and t take arguments.
+    case "${options}" in                    #
+        n)                                    # If the option is n,
+            exp=${OPTARG}                     
+            ;;
+        :)                                    # If expected argument omitted:
+            echo "Error: -${OPTARG} requires an argument."
+            exit_abnormal                       # Exit abnormally.
+            ;;
+        *)                                    # If unknown (any other) option:
+            exit_abnormal                       # Exit abnormally.
+            ;;
+    esac
+done
+if [[ $exp == "size" ]] ; then  
+    n=6
+fi
+if [[ $exp == "freq" ]] ; then   
+    n=5
+fi
+if [[ $exp == "freq2" ]] ; then   
+    n=4
+fi
+
 # exp="size"
 # n=6
 
 # exp="freq"
 # n=5
 
-exp="freq2"
-n=4
+# exp="freq2"
+# n=4
 
 protos=($(ls experiments/$exp/ | cat | grep -i "prototype.*txt"))
 docus=($(ls experiments/$exp/ | cat | grep -i "documents.*txt"))
