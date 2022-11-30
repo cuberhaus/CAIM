778551b966c20f2aa16db5662bff402eac60841b
Pol Casacuberta <polcg10@gmail.com>
2022-11-30 10:57:28 +0100

    This is a placeholder

--- sesio6/kmeans.sh
+++ sesio6/kmeans.sh
@@ -1,10 +1,16 @@
+protos=($(ls | cat | grep -i "prototype.*txt"))
+docus=($(ls | cat | grep -i "documents.*txt"))
 n=6
+
+
 for ((i = 0; i < n; i++)); do
     # echo "python generateprototypes.py --data documents$i.txt &"
-    echo "python3 MRKmeans.py  --prot prototype"$i"_8.txt --docs documents0.txt &"
+    proto=${protos[$i]}
+    docu=${docus[$i]}
+    echo "python3 MRKmeans.py  --prot $proto --docs $docu&"
     mkdir Kmeans_"$i"
     cd Kmeans_"$i"
-    python3 ../MRKmeans.py  --prot ../prototype"$i"_8.txt --docs ../documents0.txt &
+    python3 ../MRKmeans.py  --prot ../$proto --docs ../$docu &
     cd ../
 done
 
