4e451e0c80d325ac3c78eda22219af12be2b3960
Pol Casacuberta <polcg10@gmail.com>
2022-11-30 12:31:13 +0100

    This is a placeholder

--- sesio6/results.sh
+++ sesio6/results.sh
@@ -15,8 +15,8 @@ for ((j = 0; j < n_exp; j++)); do
         proto=${protos[$k]}
 
         # without the parenthesis the output was not being redirected to the corresponding file
-        echo "(python3 processresults.py --prot $expf/$folder/$proto) > $expf/$folder/processresults.txt &"
-        (python3 processresults.py --prot $expf/$folder/$proto) > $expf/$folder/processresults.txt &
+        # cmd="(python3 processresults.py --prot $expf/$folder/$proto) > $expf/$folder/processresults.txt &"
+        (set -x; (python3 processresults.py --prot $expf/$folder/$proto) > $expf/$folder/processresults.txt &)
     done
 done
 
