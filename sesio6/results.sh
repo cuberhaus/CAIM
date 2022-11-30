3e16c4462f7b82b5d9c5961a131a0ac2ac4c27a9
Pol Casacuberta <polcg10@gmail.com>
2022-11-30 12:32:33 +0100

    This is a placeholder

--- sesio6/results.sh
+++ sesio6/results.sh
@@ -15,7 +15,7 @@ for ((j = 0; j < n_exp; j++)); do
         proto=${protos[$k]}
 
         # without the parenthesis the output was not being redirected to the corresponding file
-        # cmd="(python3 processresults.py --prot $expf/$folder/$proto) > $expf/$folder/processresults.txt &"
+        # (set-x;) uses a sub-shell where every command is printed to the output
         (set -x; (python3 processresults.py --prot $expf/$folder/$proto) > $expf/$folder/processresults.txt &)
     done
 done
